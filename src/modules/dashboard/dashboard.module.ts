c6fc920313a779e7f073f5146f713e1adccff80c
Emiliano Gasco <emigasco@gmail.com>
2026-02-24 22:55:13 -0300

    feat: agregar ClientCompany al módulo y servicio de dashboard para mejorar la gestión de relaciones cliente-empresa

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -1,24 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Client, ClientSchema } from '../../schemas/client.schema';
 import {
   Transaction,
   TransactionSchema,
 } from '../../schemas/transaction.schema';
 import { Company, CompanySchema } from '../../schemas/company.schema';
 import { User, UserSchema } from '../../schemas/user.schema';
+import { Client, ClientSchema } from '../../schemas/client.schema';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
 import { SuperAdminDashboardService } from './superadmin-dashboard.service';
 import { SuperAdminDashboardController } from './superadmin-dashboard.controller';
+import {
+  ClientCompany,
+  ClientCompanySchema,
+} from '../../schemas/client-company.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Client.name, schema: ClientSchema },
       { name: Transaction.name, schema: TransactionSchema },
       { name: Company.name, schema: CompanySchema },
       { name: User.name, schema: UserSchema },
+      { name: Client.name, schema: ClientSchema },
+      { name: ClientCompany.name, schema: ClientCompanySchema },
     ]),
   ],
   providers: [DashboardService, SuperAdminDashboardService],
